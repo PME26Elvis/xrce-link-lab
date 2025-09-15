7cf5472f2cf16aada1fed7251ba58c9662275c75
Elvis_PME <elvisyen0727@gmail.com>
2025-09-15 16:23:32 +0800

    Update transport_zephyr_serial.h

--- firmware/xrce_nrf52_uxrapp/src/transport_zephyr_serial.h
+++ firmware/xrce_nrf52_uxrapp/src/transport_zephyr_serial.h
@@ -1,14 +1,12 @@
 #pragma once
-#include <uxr/client/transport/serial/serial_transport.h>
 #include <stdbool.h>
 #include <stdint.h>
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-bool uxr_init_zephyr_serial_transport(uxrSerialTransport* transport, uint32_t baudrate);
-
-#ifdef __cplusplus
-}
+#ifdef USE_UXRCLIENT
+  #include <uxr/client/transport/serial/serial_transport.h>
+  bool uxr_init_zephyr_serial_transport(uxrSerialTransport* transport, uint32_t baudrate);
+#else
+  /* 沒有 uxrclient 時此檔不會被編，但為了安全留個空宣告避免誤含 */
+  typedef struct uxrSerialTransport_ { void* platform; } uxrSerialTransport;
+  static inline bool uxr_init_zephyr_serial_transport(uxrSerialTransport* t, uint32_t b){ (void)t; (void)b; return false; }
 #endif
