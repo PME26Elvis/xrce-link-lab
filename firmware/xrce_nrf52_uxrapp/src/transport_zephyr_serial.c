1dcc6a3899a893ed17c99fb5b68b9327912e6a55
Elvis_PME <elvisyen0727@gmail.com>
2025-09-15 16:23:45 +0800

    Update transport_zephyr_serial.c

--- firmware/xrce_nrf52_uxrapp/src/transport_zephyr_serial.c
+++ firmware/xrce_nrf52_uxrapp/src/transport_zephyr_serial.c
@@ -1,4 +1,7 @@
 #include "transport_zephyr_serial.h"
+
+#ifdef USE_UXRCLIENT
+
 #include <zephyr/drivers/uart.h>
 #include <zephyr/kernel.h>
 #include <zephyr/device.h>
@@ -15,18 +18,17 @@ bool uxr_init_zephyr_serial_transport(uxrSerialTransport* transport, uint32_t ba
     zephyr_serial_platform* plat = (zephyr_serial_platform*)malloc(sizeof(*plat));
     if (!plat) return false;
 
-    /* 使用 DT_NODELABEL(uart0) 在編譯期抓到 UART0 裝置，最穩定 */
     const struct device* u = DEVICE_DT_GET(DT_NODELABEL(uart0));
     if (!device_is_ready(u)) {
         free(plat);
         return false;
     }
-
     plat->dev = u;
     transport->platform = plat;
     return true;
 }
 
+/* uxrclient 會連結以下符號 */
 bool uxr_close_serial_transport(uxrSerialTransport* transport)
 {
     if (transport && transport->platform) {
@@ -58,10 +60,11 @@ size_t uxr_read_serial_data(uxrSerialTransport* transport,
         if (uart_poll_in(plat->dev, &c) == 0) {
             buf[n++] = c;
         } else {
-            /* 小睡一下避免忙等 */
             k_sleep(K_USEC(200));
         }
     }
     if (err) *err = 0;
     return n;
 }
+
+#endif /* USE_UXRCLIENT */
